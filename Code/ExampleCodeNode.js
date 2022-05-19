874e6c14733616fb52cf2126ed29c250242ac404
Matthew 'ItsNotaJulius' Julies <104577250+itsnotajulius@users.noreply.github.com>
2022-05-20 00:19:42 +0200

    Update Code/ExampleCodeNode.js

--- Code/ExampleCodeNode.js
+++ Code/ExampleCodeNode.js
@@ -32,3 +32,11 @@ connection.query(query, function (error, results, fields) {
 connection.end(function (err) {
   // The connection is terminated now
 });
+
+var q = "SELECT CURTIME() as time, CURDATE() as date, NOW() as now";
+connection.query(q, function (error, results, fields) {
+  if (error) throw error;
+  console.log(results[0].time);
+  console.log(results[0].date);
+  console.log(results[0].now);
+});
