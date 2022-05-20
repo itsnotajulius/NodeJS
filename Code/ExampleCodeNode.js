35fedefd20cd082953a74e8586786dfbfc09ddda
Matthew 'ItsNotaJulius' Julies <104577250+itsnotajulius@users.noreply.github.com>
2022-05-20 02:12:13 +0200

    Update Code/ExampleCodeNode.js

--- Code/ExampleCodeNode.js
+++ Code/ExampleCodeNode.js
@@ -92,3 +92,38 @@ connection.query(s, function (error, results, fields) {
   }
   console.log(results.length);
 });
+
+/*INSTERTING DATA TAKE 4
+var s = "SELECT * FROM users";
+var insertStatmenmt = "INSERT INTO users (email,created_at) VALUES ?";
+var data = [];
+
+for (let index = 0; index < 500; index++) {
+  data.push([faker.internet.email(), faker.date.past()]);
+}
+
+connection.query(insertStatmenmt, data, function (error, results, fields) {
+  if (error) throw error;
+  console.log(results);
+});
+*/
+
+//INSERTING DATA TAKE 5
+var s = "SELECT * FROM users";
+var del = "DELETE FROM users";
+var insertStatmenmt = "INSERT INTO users (email,created_at) VALUES ?";
+var data = [];
+
+for (let index = 0; index < 500; index++) {
+  data.push([faker.internet.email(), faker.date.past()]);
+}
+
+connection.query(insertStatmenmt, [data], function (error, results, fields) {
+  if (error) throw error;
+  console.log(results);
+});
+
+connection.query(s, function (error, results, fields) {
+  if (error) throw error;
+  console.log(results);
+});
