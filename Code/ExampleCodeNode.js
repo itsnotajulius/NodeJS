21d63b2aef22ddf898f365d5b50e32b33388ee53
Matthew 'ItsNotaJulius' Julies <104577250+itsnotajulius@users.noreply.github.com>
2022-05-20 02:57:05 +0200

    Update Code/ExampleCodeNode.js

--- Code/ExampleCodeNode.js
+++ Code/ExampleCodeNode.js
@@ -155,7 +155,16 @@ var likie = "%yahoo%";
 
 //Getting different types of emails
 var q =
-  "SELECT CASE WHEN email LIKE ? THEN 'gmail' WHEN email LIKE ? THEN 'yahoo' WHEN email LIKE ? THEN 'hotmail' ELSE 'other' END AS provider,COUNT(*) AS total_users FROM users GROUP BY provider ORDER BY total_users DESC";
+  "SELECT CASE WHEN email LIKE ? THEN 'gmail' WHEN email LIKE ? THEN 'yahoo' WHEN email LIKE ? THEN 'hotmail' ELSE 'other' END AS provider, COUNT(*) AS total_users FROM users GROUP BY provider ORDER BY total_users DESC";
 var $gmail = "%gmail%";
 var $yahoo = "%yahoo%";
 var $hotmail = "%hotmail%";
+
+connection.query(
+  q,
+  [$gmail, $yahoo, $hotmail],
+  function (error, results, fields) {
+    if (error) throw error;
+    console.log(results);
+  }
+);
