927220229f5f14950c694a16be1ae628de88d052
Matthew 'ItsNotaJulius' Julies <104577250+itsnotajulius@users.noreply.github.com>
2022-05-21 13:04:19 +0200

    Update Projects/JoinUs/fakerMaking.js

--- Projects/JoinUs/fakerMaking.js
+++ Projects/JoinUs/fakerMaking.js
@@ -1,3 +1,15 @@
+// Making connections to the NPM faker and mysql. Look in Resources for how to set them up
+
+const { faker } = require("@faker-js/faker"); //Was in the scope/folder: faker-sj
+var mysql = require("mysql");
+
+//Connecting to database
+var connection = mysql.createConnection({
+  host: "localhost",
+  user: "root", // your root username
+  database: "join_us", // the name of your db
+});
+
 connection.connect(function (err) {
   if (err) {
     console.error("error connecting: " + err.stack);
