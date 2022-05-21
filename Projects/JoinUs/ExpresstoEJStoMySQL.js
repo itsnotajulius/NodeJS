451058785bbefd3fdf302445962e4d202d187543
Matthew 'ItsNotaJulius' Julies <104577250+itsnotajulius@users.noreply.github.com>
2022-05-22 00:29:40 +0200

    Update Projects/JoinUs/ExpresstoEJStoMySQL.js

--- Projects/JoinUs/ExpresstoEJStoMySQL.js
+++ Projects/JoinUs/ExpresstoEJStoMySQL.js
@@ -15,9 +15,8 @@ connnection.connect(function (err) {
   }
 });
 
-let q = "SELECT Count(*) AS count FROM users";
-
 app.get("/", function (req, res) {
+  let q = "SELECT Count(*) AS count FROM users";
   connnection.query(q, function (err, result, fields) {
     res.send("This ammount users " + result[0].count);
   });
