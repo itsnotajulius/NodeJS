1fbb7a9f7491e46214b9fad3e6e8c6317a07609f
Matthew 'ItsNotaJulius' Julies <104577250+itsnotajulius@users.noreply.github.com>
2022-05-21 13:03:15 +0200

    Update Projects/JoinUs/fakerMaking.js

--- Projects/JoinUs/fakerMaking.js
+++ Projects/JoinUs/fakerMaking.js
@@ -1,8 +1,32 @@
-connection.query(
-  q,
-  [$gmail, $yahoo, $hotmail],
-  function (error, results, fields) {
+
+
+connection.
+
+var s = "SELECT * FROM users";
+var del = "DELETE FROM users";
+var insertStatmenmt = "INSERT INTO users (email,created_at) VALUES ?";
+var data = [];
+
+for (let index = 0; index < 500; index++) {
+  data.push([faker.internet.email(), faker.date.past()]);
+}
+
+
+connection.query(del, function (error, results, fields) {
     if (error) throw error;
     console.log(results);
-  }
-);
+  });
+
+connection.query(insertStatmenmt, [data], function (error, results, fields) {
+  if (error) throw error;
+  console.log(results);
+});
+
+connection.query(s, function (error, results, fields) {
+  if (error) throw error;
+  console.log(results);
+});
+
+connection.end(function(){
+
+});
