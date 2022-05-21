b6d16118f0a7e02316e2e25f37a4db6238bb2720
Matthew 'ItsNotaJulius' Julies <104577250+itsnotajulius@users.noreply.github.com>
2022-05-21 13:44:09 +0200

    Update Projects/JoinUs/Queries.js

--- Projects/JoinUs/Queries.js
+++ Projects/JoinUs/Queries.js
@@ -171,3 +171,18 @@ connection.query(
     console.log(results);
   }
 );
+
+//Setting up express
+const express = require("express");
+const app = express();
+const port = 3000;
+
+//Looking for home
+app.get("/", function (reg, res) {
+  res.send("Hello APP");
+});
+
+//Listening on given port
+app.listen(port, function () {
+  console.log("App is listing");
+});
