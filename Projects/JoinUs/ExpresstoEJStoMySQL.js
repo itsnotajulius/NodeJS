ddd9394a5a0aebea9c8533874457f9cc41000983
Matthew 'ItsNotaJulius' Julies <104577250+itsnotajulius@users.noreply.github.com>
2022-05-22 00:24:16 +0200

    Update Projects/JoinUs/ExpresstoEJStoMySQL.js

--- Projects/JoinUs/ExpresstoEJStoMySQL.js
+++ Projects/JoinUs/ExpresstoEJStoMySQL.js
@@ -1,6 +1,7 @@
 let express = requrie("express");
 let mysql = requrie("mysql");
 let port = 3000;
+let app = express();
 
 let connnection = mysql.createConnection({
   host: "localhost",
@@ -8,4 +9,19 @@ let connnection = mysql.createConnection({
   database: "join_us",
 });
 
-connnection.;
+connnection.connect(function (err) {
+  if (err) throw err;
+  console.log(`Server is connecint`);
+});
+
+let q = "SELECT Count(*) AS count FROM users";
+
+app.get("/", function (req, res) {
+  connnection.query(q, function (err, result, fields) {
+    res.send("This ammount users " + result[0].count);
+  });
+});
+
+app.listen(function () {
+  console.log(`App is listening`);
+});
