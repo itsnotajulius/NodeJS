f1b1008175702f17e52a456ad516a98b9cb4e0b1
Matthew 'ItsNotaJulius' Julies <104577250+itsnotajulius@users.noreply.github.com>
2022-05-22 00:18:28 +0200

    Create Projects/JoinUs/expresstoMySQL - Copy.js

---
+++ Projects/JoinUs/expresstoMySQL - Copy.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const mysql = require("mysql");
+const { createConnection } = require("net");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  database: "join_us",
+});
+
+connection.connect(function (err) {
+  if (err) {
+    console.error("Error is:" + err.stack);
+  }
+});
+
+app.get("/", function (req, res) {
+  var q = "SELECT Count(*) AS count FROM users";
+  connection.query(q, function (error, results, fields) {
+    if (error) throw error;
+    var totalUsers = results[0].count;
+    res.send("There are " + totalUsers);
+  });
+});
+
+app.listen(function () {
+  console.log("Server is listening");
+});
