5f4275273bc4e563ff77f0ce19a8c209be0b505a
Matthew 'ItsNotaJulius' Julies <104577250+itsnotajulius@users.noreply.github.com>
2022-05-21 23:28:04 +0200

    Update Code/ExampleCodeNode.js

--- Code/ExampleCodeNode.js
+++ Code/ExampleCodeNode.js
@@ -233,3 +233,35 @@ app.get("/random_num", function (req, res) {
 app.listen(port, function () {
   console.log("App is listing");
 });
+
+///////////////////////////////////////////////////////////
+
+const express = require("express");
+const app = express();
+const mysql = require("mysql");
+const { createConnection } = require("net");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  database: "join_us",
+});
+
+connection.connect(function (err) {
+  if (err) {
+    console.error("Error is:" + err.stack);
+  }
+});
+
+app.get("/", function (req, res) {
+  var q = "SELECT Count(*) AS count FROM users";
+  connection.query(q, function (error, results, fields) {
+    if (error) throw error;
+    var totalUsers = results[0].count;
+    res.send("There are " + totalUsers);
+  });
+});
+
+app.listen(function () {
+  console.log("Server is listening");
+});
