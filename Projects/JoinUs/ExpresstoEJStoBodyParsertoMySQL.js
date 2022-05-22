291d3d63154b209b77709ea89886bb96de91ac13
Matthew 'ItsNotaJulius' Julies <104577250+itsnotajulius@users.noreply.github.com>
2022-05-22 03:57:37 +0200

    Update Projects/JoinUs/ExpresstoEJStoBodyParsertoMySQL.js

--- Projects/JoinUs/ExpresstoEJStoBodyParsertoMySQL.js
+++ Projects/JoinUs/ExpresstoEJStoBodyParsertoMySQL.js
@@ -35,7 +35,7 @@ app.post("/register", function (req, res) {
   let person = { email: req.body.email };
   connnection.query(q, person, function (err, results, fields) {
     if (err) throw err;
-    console.log(results);
+    res.render("/");
   });
 });
 
