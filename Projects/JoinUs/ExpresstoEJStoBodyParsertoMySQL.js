9739d0964194c9172de323c8a1b4a1b22d8080b4
Matthew 'ItsNotaJulius' Julies <104577250+itsnotajulius@users.noreply.github.com>
2022-05-23 16:41:07 +0200

    Update Projects/JoinUs/ExpresstoEJStoBodyParsertoMySQL.js

--- Projects/JoinUs/ExpresstoEJStoBodyParsertoMySQL.js
+++ Projects/JoinUs/ExpresstoEJStoBodyParsertoMySQL.js
@@ -8,7 +8,7 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 
 let connnection = mysql.createConnection({
-  host: "localhost",
+  host: "localhost",♣
   user: "root",
   database: "join_us",
 });
